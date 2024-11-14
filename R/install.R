90539ae1630678ce7077d7e7ba0bfba676931fd5
Jacob Clarke <jacob.clarke@pfizer.com>
2024-11-14 15:50:39 -0600

    removed auto insetall of deps

--- R/install.R
+++ R/install.R
@@ -4,7 +4,7 @@
 #' @description Installs the vendored package to the `./rvendor` directory
 install <- function(...) {
   dir.create(here::here("./rvendor"), showWarnings = FALSE, recursive = TRUE)
-  withr::with_libpaths(here::here("./rvendor"), renv::install(..., force = TRUE))
+  withr::with_libpaths(here::here("./rvendor"), renv::install(..., force = TRUE, dependencies = F))
 }
 
 #' activate
