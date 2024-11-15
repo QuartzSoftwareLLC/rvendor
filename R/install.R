585d11be9daf464082d9493c97a99fdef1c7ad36
Jacob Clarke <jacob.clarke@pfizer.com>
2024-11-15 15:13:57 -0600

    added proper install methods to install everything to project first

--- R/install.R
+++ R/install.R
@@ -3,13 +3,44 @@
 #' @export
 #' @description Installs the vendored package to the `./rvendor` directory
 install <- function(...) {
-  dir.create(here::here("./rvendor"), showWarnings = FALSE, recursive = TRUE)
-  withr::with_libpaths(here::here("./rvendor"), renv::install(..., force = TRUE, dependencies = F))
+  args <- c(...)
+  package_name <- pkgload::pkg_name(args[[1]])
+  print("Installing Vendored Packages")
+  dir.create("./rvendor", showWarnings = FALSE, recursive = TRUE)
+  # remove package from ignored list
+  ignored <- renv::settings$ignored.packages()
+  ignored <- ignored[ignored != package_name]
+  renv::settings$ignored.packages(ignored, persist = T)
+
+  # remove rvendor from the path in case it has already been activated
+  old_lib_path <- .libPaths()
+  new_lib_path  <- old_lib_path[!grepl("rvendor", old_lib_path)]
+  .libPaths(new_lib_path)
+
+  renv::install(...)
+
+  .libPaths(old_lib_path)
+
+  # renv install path
+  renv_install_path <- paste0(renv::paths$library(), "/", package_name)
+  rvendor_install_path <- paste0("rvendor/", package_name)
+
+
+  renv::settings$ignored.packages(package_name, persist = T)
+
+  #delete the package from the revndor library
+  unlink(Sys.glob(paste0("rvendor/", package_name)), recursive = T)
+  file.rename(renv_install_path, rvendor_install_path)
+  renv::settings$ignored.packages(package_name, persist = T)
 }
 
 #' activate
 #' @export
 #' @description Activates the vendored package by adding the `./rvendor` directory to the search path
 activate <- function() {
-  .libPaths(c(here::here("./rvendor"), .libPaths()))
+  .libPaths(c("./rvendor", .libPaths()))
 }
+
+
+
+
---
+++ tests/testthat/mockWithDeps/R/hello_world.R
@@ -0,0 +1,5 @@
+#' @export
+#' @import mime
+hello_world <- function() {
+  "hello"
+}
