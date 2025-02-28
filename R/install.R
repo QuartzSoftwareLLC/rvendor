944a52fa141c40eeaa3dd3e3ce618c2bbfafac34
Jacob Clarke <jacob.clarke@pfizer.com>
2025-02-28 09:54:16 -0600

    updated the ignored package list settings

--- R/install.R
+++ R/install.R
@@ -16,6 +16,8 @@ install <- function(...) {
   dir.create("./rvendor", showWarnings = FALSE, recursive = TRUE)
 
 
+
+
   # remove rvendor from the path in case it has already been activated
   old_lib_path <- .libPaths()
   new_lib_path  <- old_lib_path[!grepl("rvendor", old_lib_path)]
@@ -32,12 +34,14 @@ install <- function(...) {
   rvendor_install_path <- paste0("rvendor/", package_name)
 
 
-  renv::settings$ignored.packages(package_name, persist = T)
+  # update the ignored package list
+  ignored <- renv::settings$ignored.packages()
+  ignored <- ignored[ignored != package_name]
+  renv::settings$ignored.packages(c(package_name, ignored), persist = T)
 
   #delete the package from the rvendor library
   unlink(Sys.glob(paste0("rvendor/", package_name)), recursive = T)
   file.rename(renv_install_path, rvendor_install_path)
-  renv::settings$ignored.packages(package_name, persist = T)
 }
 
 #' activate
@@ -53,8 +57,3 @@ install <- function(...) {
 activate <- function() {
   .libPaths(c(.libPaths(), "./rvendor"))
 }
-
-
-
-
-
